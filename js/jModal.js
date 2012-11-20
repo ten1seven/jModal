58a7ab1d4ea55431c4094bedf000319e098fb3e5
Jeremy Fields <jeremy.fields@viget.com>
2012-11-20 11:17:30 -0700

    Major rewrite

--- js/jModal.js
+++ js/jModal.js
@@ -1,352 +1,276 @@
 /*
-	jModal.js (a simple image and video modal)
-	version 0.2
-	(c) 2012 Jeremy Fields [jeremy.fields@viget.com]
-	released under the MIT license
+ * jModal.js (a simple image and video modal)
+ * version 0.3
+ * (c) 2012 Jeremy Fields [jeremy.fields@viget.com]
+ * released under the MIT license
+ *
+ * # usage
+ *
+ * text image:
+ * <a href="#readmore" class="jmodal">Read More</a></p>
+ *
+ * single image:
+ * <a href="http://placekitten.com/800/800" class="jmodal" data-type="image">Single Image</a></p>
+ *
+ * image gallery:
+ * <a href="http://placekitten.com/800/600" class="jmodal" data-type="image" rel="set1">Image 1</a>
+ * <a href="http://placekitten.com/600/800" class="jmodal" data-type="image" rel="set1">Image 2</a>
+ * <a href="http://placekitten.com/500/500" class="jmodal" data-type="image" rel="set1">Image 3</a>
+ *
+ * youtube video:
+ * <a href="http://www.youtube.com/watch?v=TnWDiXHYFUg&list=UUV5MN13xFPXwJX3lzbCc6gQ&index=2&feature=plpp_video" class="jmodal" data-type="video">Video</a>
+ */
 
-	# usage
-
-	single image:
-	<a href="http://placekitten.com/800/800" class="jmodal" data-type="image">Single Image</a></p>
-
-	image gallery:
-	<a href="http://placekitten.com/800/600" class="jmodal" data-type="image" rel="set1">Image 1</a>
-	<a href="http://placekitten.com/600/800" class="jmodal" data-type="image" rel="set1">Image 2</a>
-	<a href="http://placekitten.com/500/500" class="jmodal" data-type="image" rel="set1">Image 3</a>
-
-	youtube video: 
-	<a href="http://www.youtube.com/watch?v=TnWDiXHYFUg&list=UUV5MN13xFPXwJX3lzbCc6gQ&index=2&feature=plpp_video" class="jmodal" data-type="video">Video</a>
-*/
 (function(win,doc,undefined) {
-
+	
 	'use strict';
-
-	win.jModal = function(breakpoints) {
-
-		// global variables
-		var $jmodal = '';
-		var $jmodalContent = '';
-		var $selected = '';
-		var $busyloader = '';
-		var modalPadding = 16;
-		var galleryHeight = 70;
-
-		var init = function() {
-
-			$('.jmodal').bind('click.jmodal',function(e) {
-				e.preventDefault();
-
-				var $this = $(this);
-
-				showBusyLoader($('body'));
-				($this.data('type') === 'image') ? loadImage($this,loadModal) : loadVideo($this,loadModal);
-			});
-
-			// fix indexOf in ie
-			if (!Array.prototype.indexOf) {
-				Array.prototype.indexOf = function(obj, start) {
-					for (var i = (start || 0), j = this.length; i < j; i++) {
-						if (this[i] === obj) { return i; }
-					}
-					return -1;
-				}
-			}
-
-		};
-
-
-		/* image preloader
+	
+	win.jModal = function() {
+		
+		
+		/* variables
 		========================================================================== */
-		var loadImage = function(elm,callback) {
-			var $this = elm;
-			var img = document.createElement('img');
-
-			img.onload = function() {
-				callback(img,elm);
-			};
-			img.src = $this.attr('href');
+		var $jmodal, $jUI, $sel;
+		
+		
+		/* init
+		========================================================================== */
+		var init = function() {
+			
+			$('.jmodal').on('click',function(e) {
+				e.preventDefault();
+				
+				if (!$jmodal) {
+					addModal();
+				}
+				
+				handleClick($(this));
+			});
+			
 		};
-
-
+		
+		
+		/* image
+		========================================================================== */
+		var loadImage = function(img) {
+			$jmodal.find('.jmodal-content').html(img);
+			
+			showModal();
+		};
+		
+		
 		/* youtube video
 		========================================================================== */
-		var loadVideo = function(elm,callback) {
-			var $this = elm;
-			var video = '<iframe width="640" height="390" src="http://www.youtube.com/embed/' + videoID($this.attr('href')) + '" frameborder="0" allowfullscreen></iframe>';
-
-			callback(video,$this);
+		var loadVideo = function(src) {
+			var video = '<iframe width="640" height="390" src="http://www.youtube.com/embed/' + videoID(src) + '" frameborder="0" allowfullscreen></iframe>';
+			
+			$jmodal.find('.jmodal-content').html(video);
+			
+			showModal();
 		};
-
+		
 		var videoID = function(url) {
 			var queryString = url.split('?');
 			var params = queryString[1].split('&');
 			var v = '';
-
+		
 			for (var i = 0; i < params.length; i++) {
 				if (params.indexOf('v=')) {
 					var videoParam = params[i].split('=');
 					v = videoParam[1];
-
+		
 					break;
 				}
 			}
-
+		
 			return v;
 		};
-
-
-		/* modal
+		
+		
+		/* text
 		========================================================================== */
-		var loadModal = function(elm,link) {
-
-			$jmodal = $('<div id="jmodal-modal"><div class="jmodal-content"></div><a href="#" class="jmodal-close">X</a></div>').appendTo('body');
-			$jmodalContent = $jmodal.find('.jmodal-content');
-
-			hideBusyLoader();
-			BlockUI();
-			addGalleryLinks(link);
-
-			// append element to modal
-			$jmodalContent.append(elm);
-
-			// measure container
-			var height = $jmodal.outerHeight();
-			var width = $jmodal.outerWidth();
-			var imgHeight = $jmodalContent.height();
-			var imgWidth = $jmodalContent.width();
-			var browser = winHeight();
-
-			// position modal for animation
-			$jmodal
-				.css({
-					'height': imgHeight,
-					'margin-left': ((width/2) * -1),
-					'margin-top': 0,
-					'top': ((height * -1) - 10),
-
-					'width': imgWidth
-				})
-				.delay(400)
-				.animate({
-					'top': ((browser - height)/2)
-				},300,function() {
-					closeModal($jmodal);
-
-					$jmodal
-						.css({
-							'top': '50%',
-							'margin-top': ((height/2) * -1)
-						});
-				});
+		var loadText = function(src) {
+			var text = $(src).html();
+			
+			$jmodal.find('.jmodal-content').addClass('text').html(text);
+			showModal();
 		};
-
-		var closeModal = function(modal) {
-			var $jmodal = $(modal);
-			var height = $jmodal.outerHeight();
-			var browser = winHeight();
-
-			$('.jmodal-close, #UIBlock').click(function(e) {
-				e.preventDefault();
-
-				$jmodal
-					.css({
-						'margin-top': 0,
-						'top': ((browser - height)/2)
-					})
-					.animate({
-						'top': ((height * -1) - 10)
-					},300,function() {
-						$jmodal.remove();
-						unBlockUI();
-					});
-			});
-		};
-
-
+		
+		
 		/* gallery
 		========================================================================== */
 		var addGalleryLinks = function(elm) {
-
-			var galleryContent = '';
-			if ($(elm).attr('rel')) {
-
-				var i = 1;
-				$('[rel=' + $(elm).attr('rel') + ']').each(function() {
-
-					var selectedLink = ($(this).attr('href') === $(elm).attr('href')) ? 'selected' : '';
-					galleryContent += '<a href="' + $(this).attr('href') + '" class="jmodal-gallery-link ' + selectedLink + '">' + i + '</a>';
-
-					i++;
-				});
-				$jmodal
-					.addClass('jmodal-modal-gallery')
-					.append('<p class="jmodal-gallery-links"><a href="#" class="jmodal-gallery-prev">Prev</a>' + galleryContent + '<a href="#" class="jmodal-gallery-next">Next</a></p>');
-
-				$selected = $jmodal.find('.selected');
-				bindGalleryLinks();
+			var links = '';
+			var i = 1;
+			$('[rel=' + elm.attr('rel') + ']').each(function() {
+				var selectedLink = ($(this).attr('href') === elm.attr('href')) ? 'selected' : '';
+				links += '<a href="' + $(this).attr('href') + '" class="jmodal-gallery-link ' + selectedLink + '">' + i + '</a>';
+				i++;
+			});
+			
+			$jmodal
+				.addClass('jmodal-modal-gallery')
+				.append('<p class="jmodal-gallery-links"><a href="#" class="jmodal-gallery-prev">Prev</a>' + links + '<a href="#" class="jmodal-gallery-next">Next</a></p>');
+	
+			$sel = $jmodal.find('.selected');
+			bindGalleryLinks();
+		};
+		
+		var bindGalleryLinks = function() {
+			$('.jmodal-gallery-link, .jmodal-gallery-prev, .jmodal-gallery-next').on('click',function(e) {
+				e.preventDefault();
+				
+				advanceGallery($(this));
+			});
+		};
+		
+		var advanceGallery = function(elm) {
+			if (elm.hasClass('jmodal-gallery-prev')) {
+				var $prev = $sel.prev().not(elm);
+				if ($prev.length !== 0) {
+					handleGalleryClick($prev);
+				}
+			} else if (elm.hasClass('jmodal-gallery-next')) {
+				var $next = $sel.next().not(elm);
+				if ($next.length !== 0) {
+					handleGalleryClick($next);
+				}
+			} else {
+				if (!elm.hasClass('selected')) {
+					handleGalleryClick(elm);
+				}
 			}
 		};
-
-		var bindGalleryLinks = function() {
-			$('.jmodal-gallery-link').click(function(e) {
-				e.preventDefault();
-
-				var $this = $(this);
-				if (!$this.hasClass('selected')) {
-					advanceGallery($this);
-				}
-			});
-
-			$('.jmodal-gallery-prev').click(function(e) {
-				e.preventDefault();
-
-				var $prev = $selected.prev().not(this);
-				if ($prev.length !== 0) {
-					advanceGallery($prev);
-				}
-			});
-
-			$('.jmodal-gallery-next').click(function(e) {
-				e.preventDefault();
-
-				var $next = $selected.next().not(this);
-				if ($next.length !== 0) {
-					advanceGallery($next);
-				}
-			});
-		};
-
-		var advanceGallery = function(elm) {
-			$jmodalContent
-				.find('img')
-				.animate({
+		
+		var handleGalleryClick = function(elm) {
+			updateSelected(elm);
+			
+			$jmodal
+				.find('.jmodal-content').addClass('loading')
+				.find('img').animate({
 					'opacity': 0
 				},100);
-			loadImage(elm,dispGalleryImg);
+			
+			preloadImage(elm.attr('href'),dispGalleryImg);
 		};
-
-		var dispGalleryImg = function(img,link) {
-			$jmodalContent.empty();
-
-			$selected.removeClass('selected');
-			$selected = $(link).addClass('selected');
-
+		
+		var updateSelected = function(elm) {
+			$sel.removeClass('selected');
+			$sel = elm.addClass('selected');
+		};
+		
+		var dispGalleryImg = function(img) {
+			var $content = $jmodal.find('.jmodal-content').removeClass('loading').empty();
+		
 			var $img = $(img);
 			$img
-				.appendTo($jmodalContent)
+				.appendTo($content)
 				.css({
 					'opacity': 0
 				});
-
-			var newHeight = $img.height();
-			var newWidth = $img.width();
-
-			$jmodal
-				.animate({
-					'height': newHeight,
-					'margin-left': ((newWidth + (modalPadding*2))/2) * -1,
-					'margin-top': ((newHeight + (modalPadding+galleryHeight))/2) * -1,
-					'width': newWidth
-				},300,function() {
-					$img.animate({
-						'opacity': 1
-					},300);
-				});
+			
+			var w = $jmodal.outerWidth();
+			var h = $jmodal.outerHeight();
+		
+			$jmodal.css({
+				'margin-top': marginOffset(h),
+				'margin-left': marginOffset(w)
+			});
+				
+			$img.animate({
+				'opacity': 1
+			},300);
 		};
-
-
-		/* ui block
+		
+		
+		/* show/hide modal
 		========================================================================== */
-		var BlockUI = function() {
-			if ($("#UIBlock").length === 0) {
-				$('<div id="UIBlock" />')
+		var handleClick = function(elm) {
+			if (elm.data('type') === 'image') {
+				
+				if (elm.attr('rel')) {
+					addGalleryLinks(elm);
+				}
+				
+				preloadImage(elm.attr('href'),loadImage);
+			} else if (elm.data('type') === 'video') {
+				loadVideo(elm.attr('href'));
+			} else {
+				loadText(elm.attr('href'));
+			}
+		};
+		
+		var addModal = function() {
+			$jmodal = $('<div id="jmodal"><div class="jmodal-content"></div><a href="#" class="jmodal-close">Close</a></div>').appendTo('body');
+			
+			$('.jmodal-close').on('click',function(e) {
+				e.preventDefault();
+				
+				closeModal();
+			});
+		};
+		
+		var showModal = function() {
+			var w = $jmodal.outerWidth();
+			var h = $jmodal.outerHeight();
+			
+			$jmodal.css({
+				'margin-top': marginOffset(h),
+				'margin-left': marginOffset(w)
+			}).addClass('show');
+			
+			blockUI();
+		};
+		
+		var closeModal = function() {
+			$jmodal.removeClass('show');
+			unBlockUI();
+			
+			window.setTimeout(function() {
+				$jmodal.removeClass('jmodal-modal-gallery').find('.jmodal-content').removeClass('text');
+				$jmodal.find('.jmodal-gallery-links').remove();
+			},500);
+		};
+		
+		
+		/* utility functions
+		========================================================================== */
+		var preloadImage = function(src,callback) {
+			var img = document.createElement('img');
+			img.onload = function() {
+				callback(img);
+			};
+			img.src = src;
+		};
+		
+		var marginOffset = function(m) {
+			return (m/2) * -1;
+		};
+		
+		var blockUI = function() {
+			if (!$jUI) {
+				$jUI = $('<div id="UIBlock" />')
 					.css({
-						'background': '#000',
-						'height': '100%',
-						'left': 0,
-						'opacity': 0,
-						'position': 'fixed',
-						'top': 0,
-						'width': '100%',
-						'z-index': 900
+						'opacity': 0
 					})
 					.appendTo('body')
 					.animate({
-						'opacity': 0.8
-					},180);
-				if ($.browser.msie && ($.browser.version < 7)) {
-					$('#UIBlock').css({
-						'position': 'absolute',
-						'top': $(window).scrollTop()
+						'opacity': 0.6
+					},200)
+					.on('click',function() {
+						closeModal();
 					});
-				}
 			}
 		};
-
+		
 		var unBlockUI = function() {
-			$('#UIBlock')
-				.fadeOut(180,function() {
-					$(this).remove();
-				});
+			$jUI.fadeOut(200,function() {
+				$(this).remove();
+				$jUI = '';
+			});
 		};
-
-
-		/* busy loader
-		========================================================================== */
-		var showBusyLoader = function(elm) {
-			$busyloader = $('<div id="modal-busyloader"></div>')
-				.appendTo(elm)
-				.css({
-					'opacity': 0,
-					'margin-top': -50
-				})
-				.animate({
-					'opacity': 1,
-					'margin-top': -30
-				},300);
-		};
-
-		var hideBusyLoader = function() {
-			$busyloader
-				.animate({
-					'opacity': 0,
-					'margin-top': -50
-				},200,function() {
-					$busyloader.remove();
-				});
-		};
-
-
-		/* utilities
-		========================================================================== */
-
-		// cross browser window height
-		var winHeight = function() {
-
-			var h = 0;
-
-			// IE
-			if (!window.innerHeight) {
-
-				if (!(document.documentElement.clientHeight === 0)) {
-
-					// strict mode
-					h = document.documentElement.clientHeight;
-				} else {
-
-					// quirks mode
-					h = document.body.clientHeight;
-				}
-			} else {
-
-				// w3c
-				h = window.innerHeight;
-			}
-
-			return h;
-		};
-
-
+		
+	
 		/* init
 		========================================================================== */
 		init();
